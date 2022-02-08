99b59a2a4137a2e038865f522583d59c69384d67
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2022-02-08 22:13:55 +0900

    Re-indent

--- Program.fs
+++ Program.fs
@@ -9,40 +9,40 @@ open Avalonia.FuncUI.Elmish
 open Avalonia.FuncUI.Components.Hosts
 
 type MainWindow() as this =
-    inherit HostWindow()
-    do
-        base.Title <- "Conceal"
-        base.Width <- 400.0
-        base.Height <- 400.0
-        
-        //this.VisualRoot.VisualRoot.Renderer.DrawFps <- true
-        //this.VisualRoot.VisualRoot.Renderer.DrawDirtyRects <- true
+  inherit HostWindow()
+  do
+    base.Title <- "Conceal"
+    base.Width <- 400.0
+    base.Height <- 400.0
+    
+    //this.VisualRoot.VisualRoot.Renderer.DrawFps <- true
+    //this.VisualRoot.VisualRoot.Renderer.DrawDirtyRects <- true
 
 
-        Elmish.Program.mkSimple (fun () -> Counter.init) Counter.update Counter.view
-        |> Program.withHost this
-        |> Program.run
+    Elmish.Program.mkSimple (fun () -> Counter.init) Counter.update Counter.view
+    |> Program.withHost this
+    |> Program.run
 
-        
+    
 type App() =
-    inherit Application()
+  inherit Application()
 
-    override this.Initialize() =
-        this.Styles.Load "avares://Avalonia.Themes.Default/DefaultTheme.xaml"
-        this.Styles.Load "avares://Avalonia.Themes.Default/Accents/BaseDark.xaml"
+  override this.Initialize() =
+    this.Styles.Load "avares://Avalonia.Themes.Default/DefaultTheme.xaml"
+    this.Styles.Load "avares://Avalonia.Themes.Default/Accents/BaseDark.xaml"
 
-    override this.OnFrameworkInitializationCompleted() =
-        match this.ApplicationLifetime with
-        | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime ->
-            desktopLifetime.MainWindow <- MainWindow()
-        | _ -> ()
+  override this.OnFrameworkInitializationCompleted() =
+    match this.ApplicationLifetime with
+    | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime ->
+      desktopLifetime.MainWindow <- MainWindow()
+    | _ -> ()
 
 module Program =
 
-    [<EntryPoint>]
-    let main(args: string[]) =
-        AppBuilder
-            .Configure<App>()
-            .UsePlatformDetect()
-            .UseSkia()
-            .StartWithClassicDesktopLifetime(args)
+  [<EntryPoint>]
+  let main(args: string[]) =
+    AppBuilder
+      .Configure<App>()
+      .UsePlatformDetect()
+      .UseSkia()
+      .StartWithClassicDesktopLifetime(args)
